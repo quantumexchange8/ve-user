be0e1abd85bcab115c6dc8909711634c8cc12453
unknown <demondrake00@gmail.com>
2025-07-24 11:48:55 +0800

    Fix:Ensure register will got role user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,6 +42,9 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        // Assign role
+        $user->assignRole('user');
+
         event(new Registered($user));
 
         Auth::login($user);
