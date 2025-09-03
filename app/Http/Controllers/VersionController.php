70cd20670876a9076029de85145060e292ede826
unknown <demondrake00@gmail.com>
2025-09-03 15:49:19 +0800

    Update VersionController.php

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Code;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Models\SettingLicense;
@@ -18,16 +19,23 @@ class VersionController extends Controller
 
     public function getVersionHistory(Request $request)
     {
-        $versionHistory = SettingLicense::whereHas('versionControls')
-            ->with([
-                'versionControls' => function ($query) {
-                    $query->select('id', 'setting_license_id', 'version', 'remarks', 'download_url')
-                        ->latest('id');
-                }
-            ])
+        $userId = Auth::id();
+    
+        // Step 1: Get distinct licenses that the user redeemed
+        $redeemedLicenseSlugs = Code::where('user_id', $userId)
+            ->where('status', 'redeemed')
+            ->distinct()
+            ->pluck('license_name');
+    
+        // Step 2: Get those licenses along with all their versions
+        $versionHistory = SettingLicense::whereIn('slug', $redeemedLicenseSlugs)
+            ->with(['versionControls' => function ($query) {
+                $query->select('id', 'setting_license_id', 'version', 'remarks', 'download_url')
+                      ->latest('id');
+            }])
             ->select('id', 'name', 'shortform')
             ->get();
-
+    
         return response()->json([
             'success' => true,
             'data' => $versionHistory,
